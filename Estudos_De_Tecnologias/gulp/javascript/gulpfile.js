34c199f4d83f96e59fa07333f19060899ac1547c
joao99sb <joaovictor665@gmial.com>
2020-10-26 16:06:29 -0300

    .

---
+++ Estudos_De_Tecnologias/gulp/javascript/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+
+function padrao(cb){
+
+    gulp.src('src/**/*.js')
+    .pipe(babel({
+        comments:false,
+        presets:["env"],
+
+    }))
+    .pipe(uglify())
+    .pipe(concat('codigo.min.js'))
+    .pipe(gulp.dest('build'))
+
+
+    return cb()
+}
+module.exports ={
+        default: gulp.series(padrao)
+    }
